001304c8dd42435dfe00f3dc98c8c0ab564c80d1
Alexander <alexander@anak.dk>
2022-03-21 09:45:59 +0100

    added catkin_ws to repo

---
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/alexander/Documents/catkin_ws/src'
+whitelisted_packages = 'localization_data_pub'.split(';') if 'localization_data_pub' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/opt/ros/melodic'.split(';') if '/opt/ros/melodic' != '' else []
