45c6652a5772538d917237165a211ceb51227b10
Alexander <alexander@anak.dk>
2022-08-13 13:24:14 +0200

    publishing image with features and mask

--- catkin_ws/src/line_follower/src/follow_lin.py
+++ catkin_ws/src/line_follower/src/follow_lin.py
@@ -7,22 +7,33 @@ __author__  = "Arjun S Kumar"
 import rospy
 import cv2
 import cv_bridge
-import numpy
-from sensor_msgs.msg import Image, CameraInfo
+import numpy as np
+from sensor_msgs.msg import Image, CameraInfo, CompressedImage
 from geometry_msgs.msg import Twist
 
 class Follower:
   def __init__(self):
     self.bridge = cv_bridge.CvBridge()
     #cv2.namedWindow("window", 1)
-    self.image_sub = rospy.Subscriber('/main_camera/image_raw', 
-                                      Image, self.image_callback)
-    self.cmd_vel_pub = rospy.Publisher('/cmd_vel',
-                                       Twist, queue_size=1)
+    self.image_sub = rospy.Subscriber('/main_camera/image_raw', Image, self.image_callback)
+    #self.image_comp_sub = rospy.Subscriber('/main_camera/image_raw/compressed', CompressedImage, self.image_callback)
+
+
+    self.image_comp_pub = rospy.Publisher('/main_camera/image_raw/features/compressed',CompressedImage, queue_size = 1)
+    self.mask_comp_pub = rospy.Publisher('/main_camera/image_raw/mask/compressed',CompressedImage, queue_size = 1)
+    self.cmd_vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
     self.twist = Twist()
-    self.imnumber = 0
+
+   #self.upperThreshold = rospy.set_param('image_proc/upperThreshold', 255)
+  #self.lowerThreshold = rospy.set_param('/image_proc/lowerThreshold', 200)
+
+
   def image_callback(self, msg):
-    image = self.bridge.imgmsg_to_cv2(msg,desired_encoding='bgr8')
+
+    #self.upperThreshold = rospy.get_param('image_proc/upperThreshold')
+    #self.lowerThreshold = rospy.get_param('image_proc/lowerThreshold')
+
+    self.image = self.bridge.imgmsg_to_cv2(msg,desired_encoding='bgr8')
 
     #image = cv2.rotate(image, cv2.ROTATE_90_COUNTERCLOCKWISE) 
     #hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV) 
@@ -31,11 +42,11 @@ class Follower:
     #upper_yellow = numpy.array([255, 255, 250])
     #mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
 
-    img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    img_gray = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
     img_gray = cv2.GaussianBlur(img_gray, (7, 7), 0)
-    (T, mask) = cv2.threshold(img_gray, 120, 255,cv2.THRESH_BINARY)
+    (T, mask) = cv2.threshold(img_gray, 200, 255,cv2.THRESH_BINARY)
     
-    h, w, d = image.shape
+    h, w, d = self.image.shape
     search_top = 3*h/4
     search_bot = 3*h/4 + 20
     mask[0:search_top, 0:w] = 0
@@ -44,20 +55,32 @@ class Follower:
     if M['m00'] > 0:
       cx = int(M['m10']/M['m00'])
       cy = int(M['m01']/M['m00'])
-      cv2.circle(image, (cx, cy), 20, (0,0,255), -1)
+      cv2.circle(self.image, (cx, cy), 20, (0,0,255), -1)
       # CONTROL starts
       err = cx - w/2
       self.twist.linear.x = 0.2
       self.twist.angular.z = -float(err) / 80
-      #self.cmd_vel_pub.publish(self.twist)
-      #cv2.imwrite("image_"+ str(self.imnumber)+ ".png", image)
-      #cv2.imwrite("mask_"+ str(self.imnumber)+ ".png", mask)
+
       rospy.logdebug("z error %f", -float(err) / 80)
-      self.imnumber += 1
+      self.cmd_vel_pub.publish(self.twist)
+
+
+
+      msg = CompressedImage()
+      msg.header.stamp = rospy.Time.now()
+      msg.format = "jpeg"
+      msg.data = np.array(cv2.imencode('.jpg', self.image)[1]).tostring()
+
+      mask_pub = CompressedImage()
+      mask_pub.header.stamp = rospy.Time.now()
+      mask_pub.format = "jpeg"
+      mask_pub.data = np.array(cv2.imencode('.jpg', mask)[1]).tostring()
+
+
+      self.image_comp_pub.publish(msg)
+      self.mask_comp_pub.publish(mask_pub)
       # CONTROL ends
-    #cv2.imshow("mask",mask)
-    #cv2.imshow("output", image)
-    #cv2.waitKey(3)
+
 
 rospy.init_node('follower', log_level=rospy.DEBUG)
 follower = Follower()
